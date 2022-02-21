dba8ea7769b2c7a2ac695429323df4b7a5800ad4
hardes1 <ustinovgo@gmail.com>
2022-02-21 17:49:33 +0300

    fix bugs 2

---
+++ app/build/tmp/kapt3/stubs/debug/com/example/myapplication/network/ApiKt.java
@@ -0,0 +1,16 @@
+package com.example.myapplication.network;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 2, d1 = {"\u0000\u0010\n\u0000\n\u0002\u0010\u000e\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0003\"\u000e\u0010\u0000\u001a\u00020\u0001X\u0086T\u00a2\u0006\u0002\n\u0000\"\u0011\u0010\u0002\u001a\u00020\u0003\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0004\u0010\u0005\u00a8\u0006\u0006"}, d2 = {"BASE_URL", "", "retrofit", "Lretrofit2/Retrofit;", "getRetrofit", "()Lretrofit2/Retrofit;", "app_debug"})
+public final class ApiKt {
+    @org.jetbrains.annotations.NotNull()
+    public static final java.lang.String BASE_URL = "yandex.ru";
+    @org.jetbrains.annotations.NotNull()
+    private static final retrofit2.Retrofit retrofit = null;
+    
+    @org.jetbrains.annotations.NotNull()
+    public static final retrofit2.Retrofit getRetrofit() {
+        return null;
+    }
+}
---
+++ app/build/tmp/kapt3/stubs/debug/com/example/myapplication/ui/rewards/RewardsFragment.java
@@ -0,0 +1,44 @@
+package com.example.myapplication.ui.rewards;
+
+import java.lang.System;
+
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000L\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\b\u0002\n\u0002\u0018\u0002\n\u0002\b\u0004\n\u0002\u0018\u0002\n\u0002\b\u0005\n\u0002\u0010 \n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\b\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010\u0002\n\u0002\b\u0003\u0018\u00002\u00020\u0001B\u0005\u00a2\u0006\u0002\u0010\u0002J\u0016\u0010\u000e\u001a\b\u0012\u0004\u0012\u00020\u00100\u000f2\u0006\u0010\u0011\u001a\u00020\u0012H\u0002J&\u0010\u0013\u001a\u0004\u0018\u00010\u00142\u0006\u0010\u0015\u001a\u00020\u00162\b\u0010\u0017\u001a\u0004\u0018\u00010\u00182\b\u0010\u0019\u001a\u0004\u0018\u00010\u001aH\u0016J\b\u0010\u001b\u001a\u00020\u001cH\u0016J\u001a\u0010\u001d\u001a\u00020\u001c2\u0006\u0010\u001e\u001a\u00020\u00142\b\u0010\u0019\u001a\u0004\u0018\u00010\u001aH\u0016R\u0010\u0010\u0003\u001a\u0004\u0018\u00010\u0004X\u0082\u000e\u00a2\u0006\u0002\n\u0000R\u0014\u0010\u0005\u001a\u00020\u00048BX\u0082\u0004\u00a2\u0006\u0006\u001a\u0004\b\u0006\u0010\u0007R\u001b\u0010\b\u001a\u00020\t8BX\u0082\u0084\u0002\u00a2\u0006\f\n\u0004\b\f\u0010\r\u001a\u0004\b\n\u0010\u000b\u00a8\u0006\u001f"}, d2 = {"Lcom/example/myapplication/ui/rewards/RewardsFragment;", "Landroidx/fragment/app/Fragment;", "()V", "_binding", "Lcom/example/myapplication/databinding/FragmentRewardsBinding;", "binding", "getBinding", "()Lcom/example/myapplication/databinding/FragmentRewardsBinding;", "viewModel", "Lcom/example/myapplication/ui/rewards/RewardsViewModel;", "getViewModel", "()Lcom/example/myapplication/ui/rewards/RewardsViewModel;", "viewModel$delegate", "Lkotlin/Lazy;", "generateData", "", "Lcom/example/myapplication/data/RewardsData;", "size", "", "onCreateView", "Landroid/view/View;", "inflater", "Landroid/view/LayoutInflater;", "container", "Landroid/view/ViewGroup;", "savedInstanceState", "Landroid/os/Bundle;", "onDestroyView", "", "onViewCreated", "view", "app_debug"})
+public final class RewardsFragment extends androidx.fragment.app.Fragment {
+    private com.example.myapplication.databinding.FragmentRewardsBinding _binding;
+    private final kotlin.Lazy viewModel$delegate = null;
+    
+    public RewardsFragment() {
+        super();
+    }
+    
+    private final com.example.myapplication.databinding.FragmentRewardsBinding getBinding() {
+        return null;
+    }
+    
+    private final com.example.myapplication.ui.rewards.RewardsViewModel getViewModel() {
+        return null;
+    }
+    
+    @org.jetbrains.annotations.Nullable()
+    @java.lang.Override()
+    public android.view.View onCreateView(@org.jetbrains.annotations.NotNull()
+    android.view.LayoutInflater inflater, @org.jetbrains.annotations.Nullable()
+    android.view.ViewGroup container, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+        return null;
+    }
+    
+    @java.lang.Override()
+    public void onViewCreated(@org.jetbrains.annotations.NotNull()
+    android.view.View view, @org.jetbrains.annotations.Nullable()
+    android.os.Bundle savedInstanceState) {
+    }
+    
+    @java.lang.Override()
+    public void onDestroyView() {
+    }
+    
+    private final java.util.List<com.example.myapplication.data.RewardsData> generateData(int size) {
+        return null;
+    }
+}
