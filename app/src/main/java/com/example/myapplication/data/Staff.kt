253088f2f948f759f5297ef80472b498fb14523c
hardes1 <ustinovgo@gmail.com>
2022-02-21 18:00:40 +0300

    add staff request

---
+++ app/src/main/java/com/example/myapplication/data/Staff.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.data
+
+data class Staff(
+    val company: Company,
+    val id: Int,
+    val name: String,
+    val patronymic: String,
+    val surname: String
+)
--- app/src/main/java/com/example/myapplication/network/Api.kt
+++ app/src/main/java/com/example/myapplication/network/Api.kt
@@ -1,18 +1,22 @@
 package com.example.myapplication.network
 
+import com.example.myapplication.data.StaffList
 import com.squareup.moshi.Moshi
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Query
 
-const val BASE_URL = "yandex.ru"
-
+const val BASE_URL = "http://PaperFoldingSkill.pythonanywhere.com"
+const val DEBUG_KEY = "40c43c2fec798fa2216be854101756ae"
 val retrofit: Retrofit =
     Retrofit.Builder().addConverterFactory(MoshiConverterFactory.create()).baseUrl(
         BASE_URL
     ).build()
 
 interface ApiService {
-
+    @GET("/api/staff")
+    suspend fun getStaff(@Query("key") key: String = DEBUG_KEY): StaffList
 }
 
 
